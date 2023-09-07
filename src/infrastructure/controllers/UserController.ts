386e9147954e3663f33fdd317bef0c18ae3c888f
pierreviences <imaniyazid@gmail.com>
2023-09-07 22:07:46 +0700

    feat: Add Routing

--- src/infrastructure/controllers/UserController.ts
+++ src/infrastructure/controllers/UserController.ts
@@ -1,11 +1,29 @@
 import { Request, Response } from "express";
 import { CreateUserUseCase } from "../../usecase/user/CreateUserUseCase";
+import { GetUserUseCase } from "../../usecase/user/GetUserUseCase";
+import { UpdateUserUseCase } from "../../usecase/user/UpdateUserUseCase";
+import { DeleteUserUseCase } from "../../usecase/user/DeleteUserUseCase";
+import { GetAllUsersUseCase } from "../../usecase/user/GetAllUserUseCase";
 
 export class UserController {
   private createUserUseCase: CreateUserUseCase;
+  private getUserUseCase: GetUserUseCase;
+  private updateUserUseCase: UpdateUserUseCase;
+  private deleteUserUseCase: DeleteUserUseCase;
+  private getAllUsersUseCase: GetAllUsersUseCase;
 
-  constructor(createUserUseCase: CreateUserUseCase) {
+  constructor(
+    createUserUseCase: CreateUserUseCase,
+    getUserUseCase: GetUserUseCase,
+    updateUserUseCase: UpdateUserUseCase,
+    deleteUserUseCase: DeleteUserUseCase,
+    getAllUsersUseCase: GetAllUsersUseCase
+  ) {
     this.createUserUseCase = createUserUseCase;
+    this.getUserUseCase = getUserUseCase;
+    this.updateUserUseCase = updateUserUseCase;
+    this.deleteUserUseCase = deleteUserUseCase;
+    this.getAllUsersUseCase = getAllUsersUseCase;
   }
 
   async createUser(req: Request, res: Response) {
@@ -16,6 +34,63 @@ export class UserController {
         res.status(201).json(userResponse);
       } else {
         res.status(400).json(userResponse);
+      }
+    } catch (error) {
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  }
+
+  async getUser(req: Request, res: Response) {
+    try {
+      const userId = req.params.id;
+      const userResponse = await this.getUserUseCase.execute(userId);
+      if (userResponse.success) {
+        res.status(200).json(userResponse);
+      } else {
+        res.status(404).json(userResponse);
+      }
+    } catch (error) {
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  }
+
+  async updateUser(req: Request, res: Response) {
+    try {
+      const userId = req.params.id;
+      const { name, email } = req.body;
+      const userResponse = await this.updateUserUseCase.execute(userId, {
+        name,
+        email,
+      });
+      if (userResponse.success) {
+        res.status(200).json(userResponse);
+      } else {
+        res.status(404).json(userResponse);
+      }
+    } catch (error) {
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  }
+
+  async deleteUser(req: Request, res: Response) {
+    try {
+      const userId = req.params.id;
+      const userResponse = await this.deleteUserUseCase.execute(userId);
+      if (userResponse.success) {
+        res.status(204).send();
+      } else {
+        res.status(404).json(userResponse);
+      }
+    } catch (error) {
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  }
+
+  async getAllUsers(req: Request, res: Response) {
+    try {
+      const usersResponse = await this.getAllUsersUseCase.execute();
+      if (usersResponse.success) {
+        res.status(200).json(usersResponse);
       }
     } catch (error) {
       res.status(500).json({ error: "Internal Server Error" });
--- src/infrastructure/routes/user/userRoutes.ts
+++ src/infrastructure/routes/user/userRoutes.ts
@@ -1,13 +1,31 @@
 import express from "express";
 import UserRepository from "../../repository/UserRepository";
 import { CreateUserUseCase } from "../../../usecase/user/CreateUserUseCase";
+import { GetUserUseCase } from "../../../usecase/user/GetUserUseCase";
+import { UpdateUserUseCase } from "../../../usecase/user/UpdateUserUseCase";
+import { DeleteUserUseCase } from "../../../usecase/user/DeleteUserUseCase";
 import { UserController } from "../../controllers/UserController";
+import { GetAllUsersUseCase } from "../../../usecase/user/GetAllUserUseCase";
 
 const router = express.Router();
 const userRepository = new UserRepository();
 const createUserUseCase = new CreateUserUseCase(userRepository);
-const userController = new UserController(createUserUseCase);
+const getUserUseCase = new GetUserUseCase(userRepository);
+const updateUserUseCase = new UpdateUserUseCase(userRepository);
+const deleteUserUseCase = new DeleteUserUseCase(userRepository);
+const getAllUsersUseCase = new GetAllUsersUseCase(userRepository);
+const userController = new UserController(
+  createUserUseCase,
+  getUserUseCase,
+  updateUserUseCase,
+  deleteUserUseCase,
+  getAllUsersUseCase
+);
 
 router.post("/", (req, res) => userController.createUser(req, res));
+router.get("/:id", (req, res) => userController.getUser(req, res));
+router.put("/:id", (req, res) => userController.updateUser(req, res));
+router.delete("/:id", (req, res) => userController.deleteUser(req, res));
+router.get("/", (req, res) => userController.getAllUsers(req, res));
 
 export default router;
