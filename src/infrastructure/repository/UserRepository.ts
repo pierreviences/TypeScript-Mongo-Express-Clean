1f4d86d59a7152793556565482de5a0539df6277
pierreviences <imaniyazid@gmail.com>
2023-09-07 21:52:33 +0700

    feat: Add Repository Find By Id

--- src/infrastructure/repository/UserRepository.ts
+++ src/infrastructure/repository/UserRepository.ts
@@ -22,6 +22,18 @@ class UserRepository {
     }
   }
 
+  async findById(id: string): Promise<User | null> {
+    try {
+      await this.db.connected();
+      const usersCollection = this.db.getUsersCollection();
+      const user = await usersCollection.findOne({ _id: new ObjectId(id) });
+      return user;
+    } catch (error) {
+      console.error("Error finding user:", error);
+      throw error;
+    }
+  }
+
   async closeDatabase() {
     await this.db.close();
   }
